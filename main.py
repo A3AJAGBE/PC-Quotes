4c9bf8a286020617abe31b5b80373227752f7db4
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-31 16:32:56 +0000

    Add database setup

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 # Imports
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import dbinfo
 
 # Create an instance of flask
 app = Flask(__name__)
+
+# Database setup
+dblink = 'mysql+pymysql://{0}:{1}@{2}/{3}'.format(
+    dbinfo.user, dbinfo.password, dbinfo.host, dbinfo.database)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = dblink
+db = SQLAlchemy(app)
 
 # TODO: Remove debug in production
 if __name__ == '__main__':
