72e638943c8cc6fad1717cccf7cf05f31c5b7206
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-31 16:36:49 +0000

    Add database model

--- main.py
+++ main.py
@@ -13,6 +13,16 @@ dblink = 'mysql+pymysql://{0}:{1}@{2}/{3}'.format(
 app.config['SQLALCHEMY_DATABASE_URI'] = dblink
 db = SQLAlchemy(app)
 
+
+# Database model - Creating a Quotes table
+class Quotes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    quote = db.Column(db.String(255), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"Quote: {self.quote}"
+
+
 # TODO: Remove debug in production
 if __name__ == '__main__':
     app.run(debug=True)
