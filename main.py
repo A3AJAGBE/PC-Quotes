b9fcc95e537fe9f4dbe706575378ee302e02aa0a
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-16 06:39:02 +0000

    Remove delete function

--- main.py
+++ main.py
@@ -68,13 +68,6 @@ class QuoteResource(Resource):
         db.session.commit()
         return quote_schema.dump(new_quote)
 
-    def delete(self, id):
-        """This functions deletes a quote in the database."""
-        quote = Quotes.query.get_or_404(id)
-        db.session.delete(quote)
-        db.session.commit()
-        return '', 204
-
 
 # Register resource and define endpoint
 api.add_resource(QuoteResource, '/quotes/v1.0/')
